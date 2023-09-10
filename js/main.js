bdcf8fe9b68d1fe2e5d8a2c56ae65ddd02871523
Pasley2002 <valdes.matias.2020@gmail.com>
2023-09-10 03:04:38 -0300

    Actualizado

--- js/main.js
+++ js/main.js
@@ -11,4 +11,74 @@ botonesHeader.forEach(boton => boton.addEventListener("click", (event) => {
             animal.style.display = "none";
         }
     });
-}));
+}))
+
+const botonCuenta = document.getElementById("crear_cuenta");
+
+botonCuenta.addEventListener("click", () => {
+    let respuesta = confirm("¿Quieres hacerte una cuenta en AnimalDex?");
+    verificador(respuesta);
+})
+
+function verificador(respuesta) {
+    
+    if (respuesta) {
+        let username;
+        do {
+            username = prompt("Ingrese un nombre de usuario:");
+            if (username === null) {
+                alert("Has cancelado el proceso.");
+                animales.forEach(animal => {
+                    animal.style.display = "block";
+                });
+                return;
+            }
+        } while (username === "");
+
+        let contraseña;
+        do {
+            contraseña = prompt("Ingrese una contraseña:");
+            if (contraseña === null) {
+                alert("Has cancelado el proceso.");
+                animales.forEach(animal => {
+                    animal.style.display = "block";
+                });
+                return;
+            }
+        } while (contraseña === "");
+
+        no_robots(username, contraseña);
+    } 
+    else {
+        alert("¿No deseas hacerte una cuenta en AnimalDex? Esta bien.");
+        animales.forEach(animal => {
+            animal.style.display = "block";
+        });
+    }
+}
+
+function no_robots(username, contraseña) {
+
+    let resultado = prompt("Vamos a asegurarnos que no seas un robot\n\n¿Cuánto es 2 + 2?");
+
+    if (resultado === null) {
+        alert("Has cancelado el proceso.");
+        animales.forEach(animal => {
+            animal.style.display = "block";
+        });
+        return;
+    }
+
+    if (resultado == 4) {
+        alert("Tu nombre de usuario es: " + username + "\nTu contraseña es: " + contraseña);
+        animales.forEach(animal => {
+            animal.style.display = "block";
+        });
+    } 
+    else {
+        alert("Lo sentimos, intentelo más tarde.");
+        animales.forEach(animal => {
+            animal.style.display = "block";
+        });
+    }
+}
