413ec3578486d90bccfe83a46d7c582378a08b3b
Pasley2002 <valdes.matias.2020@gmail.com>
2023-10-04 17:04:00 -0300

    Actualización

--- js/main.js
+++ js/main.js
@@ -1,84 +1,100 @@
+// Selecciona todos los botones con la clase "btn-header"
 const botonesHeader = document.querySelectorAll(".btn-header");
+// Selecciona todos los elementos con la clase "animal"
 const animales = document.querySelectorAll(".animal");
 
+// Asigna un evento de clic a cada botón del encabezado
 botonesHeader.forEach(boton => boton.addEventListener("click", (event) => {
+    // Obtiene el tipo de animal del ID del botón clicado
     const tipo = event.currentTarget.id;
 
+    // Itera sobre todos los elementos con la clase "animal"
     animales.forEach(animal => {
+        // Si es el botón "verTodos" o el animal tiene la clase del tipo clicado
         if (tipo === "verTodos" || animal.classList.contains(tipo)) {
+            // Muestra el animal
             animal.style.display = "block";
         } else {
+            // Oculta el animal
             animal.style.display = "none";
         }
     });
 }))
 
+// Selecciona el botón con el ID "crear_cuenta"
 const botonCuenta = document.getElementById("crear_cuenta");
 
+// Asigna un evento de clic al botón de cuenta
 botonCuenta.addEventListener("click", () => {
+    // Muestra un cuadro de confirmación
     let respuesta = confirm("¿Quieres hacerte una cuenta en AnimalDex?");
+    // Llama a la función verificador con la respuesta
     verificador(respuesta);
 })
 
-function verificador(respuesta) {
-    
+// Función verificador que maneja el proceso de creación de cuenta
+function verificador(respuesta) { 
     if (respuesta) {
         let username;
         do {
+            // Solicita un nombre de usuario hasta que se ingrese uno o se cancele
             username = prompt("Ingrese un nombre de usuario:");
             if (username === null) {
                 alert("Has cancelado el proceso.");
-                animales.forEach(animal => {
-                    animal.style.display = "block";
-                });
+                // Muestra todos los animales nuevamente
+                mostrarAnimales();
                 return;
             }
         } while (username === "");
 
         let contraseña;
         do {
+            // Solicita una contraseña hasta que se ingrese una o se cancele
             contraseña = prompt("Ingrese una contraseña:");
             if (contraseña === null) {
                 alert("Has cancelado el proceso.");
-                animales.forEach(animal => {
-                    animal.style.display = "block";
-                });
+                // Muestra todos los animales nuevamente
+                mostrarAnimales();
                 return;
             }
         } while (contraseña === "");
 
+        // Llama a la función no_robots con el nombre de usuario y la contraseña
         no_robots(username, contraseña);
     } 
     else {
         alert("¿No deseas hacerte una cuenta en AnimalDex? Esta bien.");
-        animales.forEach(animal => {
-            animal.style.display = "block";
-        });
+        // Muestra todos los animales nuevamente
+        mostrarAnimales();
     }
 }
 
+// Función que verifica si el usuario es un robot
 function no_robots(username, contraseña) {
 
     let resultado = prompt("Vamos a asegurarnos que no seas un robot\n\n¿Cuánto es 2 + 2?");
 
     if (resultado === null) {
         alert("Has cancelado el proceso.");
-        animales.forEach(animal => {
-            animal.style.display = "block";
-        });
-        return;
+        // Muestra todos los animales nuevamente
+        mostrarAnimales();
     }
 
     if (resultado == 4) {
-        alert("Tu nombre de usuario es: " + username + "\nTu contraseña es: " + contraseña);
-        animales.forEach(animal => {
-            animal.style.display = "block";
-        });
+        alert("Tu nombre de usuario es: " + username + "\nTu contraseña es: " + contraseña + "\n" + "\nHas creado tu cuenta de AnimalDex, cuando tengamos una base de datos podrás aprovechar más sobre tu cuenta ¡Versión demo! Igual muchas gracias :)");
+        // Muestra todos los animales nuevamente
+        mostrarAnimales();
     } 
     else {
         alert("Lo sentimos, intentelo más tarde.");
-        animales.forEach(animal => {
-            animal.style.display = "block";
-        });
+        // Muestra todos los animales nuevamente
+        mostrarAnimales();
     }
+}
+
+// Función auxiliar para mostrar todos los animales
+function mostrarAnimales() {
+    animales.forEach(animal => {
+        animal.style.display = "block";
+    });
 }
